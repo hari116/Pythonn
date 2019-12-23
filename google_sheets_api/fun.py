4ae3e8045cd407710109d16df5dd17b08ba0a657
hari <hari@protracked.in>
2019-12-23 20:34:46 +0530

    updating sheet and read seems promising

--- google_sheets_api/fun.py
+++ google_sheets_api/fun.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 import pickle
 import os.path
+from pprint import pprint
+
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -11,8 +13,10 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']  # .readonly if read o
 creds = None
 
 # The ID and range of a sample spreadsheet.
-SPREADSHEET_ID = '1Oq8CAD8Y7LSIw4Md0vy4g_83CtS6ibIB3ekQqNYdIGc'  # test.xlsx
-RANGE_NAME = 'Sheet1'
+# SPREADSHEET_ID = '1Oq8CAD8Y7LSIw4Md0vy4g_83CtS6ibIB3ekQqNYdIGc'  # test.xlsx
+SPREADSHEET_ID = '1Ib86_qGr_Lx6DKL7RjMfivlCYyjVJmVPP-Ev01vuLIk'  # oriental.xlsx
+RANGE_NAME = 'Pvt.Car!C5'
+RANGE_NAMES = ['Sheet1', 'Sheet2']
 
 
 def getCreds():
@@ -36,19 +40,17 @@ def getCreds():
             pickle.dump(creds, token)
     print('getCreds ran')
 
-# Immideately run getCred()
-getCreds()
 
-
-def readSheets():
+def readSheets(READ_RANGE):
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
     """
+    # READ_RANGE = ''
     # Starts service
-    service = build('sheets', 'v4', credentials=creds)
     # Call the sheets API
     SheetService = service.spreadsheets()
-    request = SheetService.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME)
+    request = SheetService.values().get(spreadsheetId=SPREADSHEET_ID, range=READ_RANGE)
+    # request = SheetService.values().batchGet(spreadsheetId=SPREADSHEET_ID, ranges=RANGE_NAMES)
     response = request.execute()
     values = response.get('values', [])  # =response['values']
     for row in values:
@@ -58,10 +60,22 @@ def readSheets():
     # print(values[0][0])
 
 
+def batchRead():
+    # request = service.spreadsheets().values().batchGet(spreadsheetId=spreadsheet_id, ranges=ranges, valueRenderOption=value_render_option, dateTimeRenderOption=date_time_render_option)
+    print("Batch reading")
+    request = service.spreadsheets().values().batchGet(spreadsheetId=SPREADSHEET_ID, ranges=RANGE_NAMES)
+    response = request.execute()
+    pprint(response)
+    valueRanges = response.get('valueRanges', [])
+    pprint(valueRanges[0].get('values'))
+    pprint(valueRanges[1].get('values'))
+
+
 def createSheets(title):
     """
     Creating a spread sheet
     """
+    # getCred() already ran
     print('Before create')
     # spreadsheet dict. Ref: https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets
     ss_dict = {
@@ -69,14 +83,41 @@ def createSheets(title):
             'title': title
         }
     }
-    service = build('sheets', 'v4', credentials=creds)
-    request = service.spreadsheets().create(body=ss_dict, fields='spreadsheetId')
+    request = service.spreadsheets().create(body=ss_dict, fields='spreadsheetId,spreadsheetUrl,properties')
     response = request.execute()
     print(response)
     print('Spreadsheet Id: {0}'.format(response.get('spreadsheetId')))
     print('After create')
 
 
+def updateSheets():
+    print('Begining update')
+    values = [
+        [75000]
+    ]
+    body = {
+        # 'range': 'Sheet2!A1:A2', NOT WORKING...
+        # 'majorDimension': 'COLUMNS',
+        'values': values
+    }
+    request = service.spreadsheets().values().update(
+        spreadsheetId=SPREADSHEET_ID,
+        range=RANGE_NAME,
+        valueInputOption='USER_ENTERED',
+        body=body
+    )
+    response = request.execute()
+    pprint(response)
+
+
+"""main()"""
+# Immideately run getCred()
+getCreds()
+# Initializing service obj
+service = build('sheets', 'v4', credentials=creds)  # service = discovery.build('sheets', 'v4', credentials=credentials)
+
 if __name__ == '__main__':
-    # readSheets()
-    createSheets('new20dec2')
+    updateSheets()
+    readSheets('Pvt.Car')
+    # batchRead()
+    # createSheets('new23dec4')
