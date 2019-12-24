a5d78bd8ccd9b21e3db996106a4cd6fae1fe33b4
hari <hari@protracked.in>
2019-12-24 17:56:11 +0530

    stuck at batchUpdate()

--- google_sheets_api/fun.py
+++ google_sheets_api/fun.py
@@ -13,8 +13,8 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']  # .readonly if read o
 creds = None
 
 # The ID and range of a sample spreadsheet.
-# SPREADSHEET_ID = '1Oq8CAD8Y7LSIw4Md0vy4g_83CtS6ibIB3ekQqNYdIGc'  # test.xlsx
-SPREADSHEET_ID = '1Ib86_qGr_Lx6DKL7RjMfivlCYyjVJmVPP-Ev01vuLIk'  # oriental.xlsx
+SPREADSHEET_ID = '1Oq8CAD8Y7LSIw4Md0vy4g_83CtS6ibIB3ekQqNYdIGc'  # test.xlsx
+# SPREADSHEET_ID = '1Ib86_qGr_Lx6DKL7RjMfivlCYyjVJmVPP-Ev01vuLIk'  # oriental.xlsx
 RANGE_NAME = 'Pvt.Car!C5'
 RANGE_NAMES = ['Sheet1', 'Sheet2']
 
@@ -45,11 +45,9 @@ def readSheets(READ_RANGE):
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
     """
-    # READ_RANGE = ''
-    # Starts service
     # Call the sheets API
     SheetService = service.spreadsheets()
-    request = SheetService.values().get(spreadsheetId=SPREADSHEET_ID, range=READ_RANGE)
+    request = SheetService.values().get(spreadsheetId=SPREADSHEET_ID, range=READ_RANGE, majorDimension='ROWS')
     # request = SheetService.values().batchGet(spreadsheetId=SPREADSHEET_ID, ranges=RANGE_NAMES)
     response = request.execute()
     values = response.get('values', [])  # =response['values']
@@ -93,11 +91,11 @@ def createSheets(title):
 def updateSheets():
     print('Begining update')
     values = [
-        [75000]
+        [750000]
     ]
     body = {
         # 'range': 'Sheet2!A1:A2', NOT WORKING...
-        # 'majorDimension': 'COLUMNS',
+        'majorDimension': 'COLUMNS',
         'values': values
     }
     request = service.spreadsheets().values().update(
@@ -110,6 +108,33 @@ def updateSheets():
     pprint(response)
 
 
+def batchUpdate():
+    value = [
+        [360000, 75000, 1, 1],
+        [2015, 'A', 1200]
+    ]
+    data = [
+        {
+            'range': 'Sheet2!A1:B1',
+            'values': [12, 33]
+        },
+        {
+            'range': 'Sheet2!A2:B2',
+            'values': [13, 40]
+        }
+    ]
+    body = {
+        # 'valueInputOption':
+        'majorDimension': 'COLUMNS',
+        'data': data
+    }
+    request = service.spreadsheets().values().batchUpdate(
+        spreadsheetId=SPREADSHEET_ID, body=body
+    )
+    response = request.execute()
+    pprint(response)
+
+
 """main()"""
 # Immideately run getCred()
 getCreds()
@@ -117,7 +142,9 @@ getCreds()
 service = build('sheets', 'v4', credentials=creds)  # service = discovery.build('sheets', 'v4', credentials=credentials)
 
 if __name__ == '__main__':
-    updateSheets()
-    readSheets('Pvt.Car')
+    # updateSheets()
+    batchUpdate()
+    # readSheets('Pvt.Car!B33:G36')
+    readSheets('Sheet2!A1:C2')
     # batchRead()
     # createSheets('new23dec4')
