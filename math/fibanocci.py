ae1b09e90cc806bb431c0637d38f3c387f7b2ede
hari <hari.protracked@gmail.com>
2020-01-17 13:23:34 +0530

    iterFibanocci

---
+++ math/fibanocci.py
@@ -0,0 +1,38 @@
+# iterator
+
+class NatNumIterator:
+
+    def __iter__(self):
+        self.a = 0
+        return self
+
+    # def __next__(self):
+    #     x = self.a
+    #     self.a += 1
+    #     return x
+
+    def __next__(self):
+        self.a += 1
+        return self.a
+
+
+class IterFibanocci:
+    def __iter__(self):
+        self.a = 0
+        self.b = 1
+        return self
+
+    def __next__(self):
+        x, y = self.a, self.b
+        nxt = self.a + self.b
+        self.a = self.b
+        self.b = nxt
+        return x, y
+
+
+if __name__ == '__main__':
+    inst = IterFibanocci()
+    myiter = iter(inst)
+    # print(next(myiter))
+    for i in range(100):
+        print(next(myiter)[1])
