8fd8acd7ed7092c47c71632cb6b75af4e15bb41e
hari <hari.protracked@gmail.com>
2020-02-03 15:20:18 +0530

    fibanocci in iter

--- math/fibanocci.py
+++ math/fibanocci.py
@@ -34,5 +34,5 @@ if __name__ == '__main__':
     inst = IterFibanocci()
     myiter = iter(inst)
     # print(next(myiter))
-    for i in range(100):
+    for i in range(10):
         print(next(myiter)[1])
